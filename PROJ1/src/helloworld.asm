bbaee6a3ed29c16e9957619fb11f7349d3925338
StevenM-PO <60758825+StevenM-PO@users.noreply.github.com>
2020-03-12 20:33:35 -0700

    Add files via upload

---
+++ PROJ1/src/helloworld.asm
@@ -0,0 +1,34 @@
+.include "constants.inc"
+.include "header.inc"
+
+.segment "CODE"
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  RTI
+.endproc
+
+.import reset_handler
+
+.export main
+.proc main
+forever:
+  LDX PPUSTATUS
+  LDX #$3F
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+  LDA #$11
+  STA PPUDATA
+  LDA #%00111110
+  STA PPUMASK
+  jmp forever
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "CHR"
+.res 8192
